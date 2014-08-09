cd10fa2e725942d9d4ca1f88de79d9e3244bf2d0
Thomas Mansencal <thomas.mansencal@gmail.com>
2014-08-09 16:17:03 +0200

    Update "colour.constants" package documentation.

--- colour/constants/cie.py
+++ colour/constants/cie.py
@@ -2,16 +2,10 @@
 # -*- coding: utf-8 -*-
 
 """
-**cie.py**
+CIE Constants
+=============
 
-**Platform:**
-    Windows, Linux, Mac Os X.
-
-**Description:**
-    Defines partial **Colour** package *CIE* constants.
-
-**Others:**
-
+Defines *CIE* constants.
 """
 
 from __future__ import unicode_literals
@@ -27,4 +21,47 @@ __all__ = ["CIE_E",
            "CIE_K"]
 
 CIE_E = 216. / 24389.0
+"""
+*CIE* :math:`\epsilon` constant.
+
+CIE_E : float
+
+Notes
+-----
+-   The original *CIE* value for :math:`\epsilon` is :math:`\epsilon=0.008856`,
+    **Bruce Lindbloom** has shown that this value is causing a discontinuity
+    a the junction point of the two functions grafted together to create the
+    *Lightness* :math:`L^*` function.
+
+    That discontinuity can be avoided by using the rational representation as
+    follows: :math:`\epsilon=216/24389`.
+
+References
+----------
+.. [1]  http://brucelindbloom.com/index.html?LContinuity.html
+        (Last accessed 24 February 2014)
+
+"""
+
 CIE_K = 24389. / 27.0
+"""
+*CIE* :math:`k` constant.
+
+CIE_K : float
+
+Notes
+-----
+-   The original *CIE* value for :math:`k` is :math:`k=903.3`,
+    **Bruce Lindbloom** has shown that this value is causing a discontinuity
+    a the junction point of the two functions grafted together to create the
+    *Lightness* :math:`L^*` function.
+
+    That discontinuity can be avoided by using the rational representation as
+    follows: :math:`k=24389/27`.
+
+References
+----------
+.. [2]  http://brucelindbloom.com/index.html?LContinuity.html
+        (Last accessed 24 February 2014)
+
+"""
--- colour/constants/codata.py
+++ colour/constants/codata.py
@@ -2,16 +2,11 @@
 # -*- coding: utf-8 -*-
 
 """
-**codata.py**
+Fundamental Physical Constants
+==============================
 
-**Platform:**
-    Windows, Linux, Mac Os X.
-
-**Description:**
-    Defines partial **Colour** package *CODATA* constants.
-
-**Others:**
-
+Defines various constants from recommended values by the Committee on Data for
+Science and Technology (CODATA).
 """
 
 from __future__ import unicode_literals
@@ -28,5 +23,22 @@ __all__ = ["LIGHT_SPEED",
            "BOLTZMANN_CONSTANT"]
 
 LIGHT_SPEED = 299792458
+"""
+Speed of light in vacuum.
+
+LIGHT_SPEED : float
+"""
+
 PLANCK_CONSTANT = 6.62607e-34
+"""
+Planck constant.
+
+PLANCK_CONSTANT : float
+"""
+
 BOLTZMANN_CONSTANT = 1.38065e-23
+"""
+Boltzmann constant.
+
+BOLTZMANN_CONSTANT : float
+"""
