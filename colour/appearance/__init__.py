5faf0b432549db5be3b809e929802c87b20a3e79
Thomas Mansencal <thomas.mansencal@gmail.com>
2014-08-14 21:25:06 +0200

    Update "colour.appearance.ciecam02" module documentation.

--- colour/appearance/__init__.py
+++ colour/appearance/__init__.py
@@ -4,14 +4,14 @@
 from __future__ import absolute_import
 
 from .atd95 import XYZ_to_ATD95
-from .ciecam02 import XYZ_to_CIECAM02, CIECAM02_to_XYZ
+from .ciecam02 import CIECAM02_Specification, XYZ_to_CIECAM02, CIECAM02_to_XYZ
 from .hunt import XYZ_to_Hunt
 from .llab import XYZ_to_LLAB
 from .nayatani95 import XYZ_to_Nayatani95
 from .rlab import XYZ_to_RLAB
 
 __all__ = ['XYZ_to_ATD95']
-__all__ += ['XYZ_to_CIECAM02', 'CIECAM02_to_XYZ']
+__all__ += ['CIECAM02_Specification', 'XYZ_to_CIECAM02', 'CIECAM02_to_XYZ']
 __all__ += ['XYZ_to_Hunt']
 __all__ += ['XYZ_to_LLAB']
 __all__ += ['XYZ_to_Nayatani95']
