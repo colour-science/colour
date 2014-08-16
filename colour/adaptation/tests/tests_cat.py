4285f037abaae16c4e2b776a48c128b9b3033bcf
Thomas Mansencal <thomas.mansencal@gmail.com>
2014-08-16 18:29:11 +0200

    Fix header in "colour.adaptation.cat" module.

--- colour/adaptation/tests/tests_cat.py
+++ colour/adaptation/tests/tests_cat.py
@@ -17,11 +17,11 @@ else:
 
 from colour.adaptation import get_chromatic_adaptation_matrix
 
-__author__ = 'Thomas Mansencal'
-__copyright__ = 'Copyright (C) 2013 - 2014 - Thomas Mansencal'
-__license__ = 'GPL V3.0 - http://www.gnu.org/licenses/'
-__maintainer__ = 'Thomas Mansencal'
-__email__ = 'thomas.mansencal@gmail.com'
+__author__ = 'Colour Developers'
+__copyright__ = 'Copyright (C) 2013 - 2014 - Colour Developers'
+__license__ = 'New BSD License - http://opensource.org/licenses/BSD-3-Clause'
+__maintainer__ = 'Colour Developers'
+__email__ = 'colour-science@googlegroups.com'
 __status__ = 'Production'
 
 __all__ = ['TestGetChromaticAdaptationMatrix']
