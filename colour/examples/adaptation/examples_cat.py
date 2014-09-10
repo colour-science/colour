b1dd3815bdec475159d398ef35f3e9693442196c
Thomas Mansencal <thomas.mansencal@gmail.com>
2014-09-10 08:26:04 +0200

    Update "CAT" examples.

--- colour/examples/adaptation/examples_cat.py
+++ colour/examples/adaptation/examples_cat.py
@@ -12,28 +12,29 @@ from colour.utilities.verbose import message_box
 
 message_box('Chromatic Adaptation Computations')
 
-XYZ1 = (1.09923822, 1.000, 0.35445412)
-XYZ2 = (0.96907232, 1.000, 1.121792157)
+XYZ_w = (1.09846607, 1., 0.3558228)
+XYZ_wr = (1.09846607, 1., 0.3558228)
 message_box(('Computing the chromatic adaptation matrix from two source '
              '"CIE XYZ" matrices, default CAT is "CAT02".\n'
-             '\n\t"XYZ1":\n\t\t{0}\n\t"XYZ2":\n\t\t{1}'.format(XYZ1, XYZ2)))
-print(colour.chromatic_adaptation_matrix(XYZ1, XYZ2))
+             '\n\t"XYZ_w":\n\t\t{0}\n\t"XYZ_wr":\n\t\t{1}'.format(
+    XYZ_w, XYZ_wr)))
+print(colour.chromatic_adaptation_matrix(XYZ_w, XYZ_wr))
 
 print('\n')
 
 message_box('Using "Bradford" CAT.')
-print(colour.chromatic_adaptation_matrix(XYZ1, XYZ2, method='Bradford'))
+print(colour.chromatic_adaptation_matrix(XYZ_w, XYZ_wr, method='Bradford'))
 
 print('\n')
 
 message_box(('Computing the chromatic adaptation matrix from '
              '"CIE Standard Illuminant A" to '
-             '"CIE Standard Illuminant D Series D60" using "Von Kries" CAT.'))
+             '"CIE Standard Illuminant D Series D65" using "Von Kries" CAT.'))
 A = colour.ILLUMINANTS['CIE 1931 2 Degree Standard Observer']['A']
-D60 = colour.ILLUMINANTS['CIE 1931 2 Degree Standard Observer']['D60']
+D65 = colour.ILLUMINANTS['CIE 1931 2 Degree Standard Observer']['D65']
 print(colour.chromatic_adaptation_matrix(
     colour.xy_to_XYZ(A),
-    colour.xy_to_XYZ(D60),
+    colour.xy_to_XYZ(D65),
     method='Von Kries'))
 
 print('\n')
@@ -41,10 +42,10 @@ print('\n')
 XYZ = [1.14176346, 1., 0.49815206]
 message_box(('Adapting given "CIE XYZ" matrix from '
              '"CIE Standard Illuminant A" to '
-             '"CIE Standard Illuminant D Series D60" using "Sharp" CAT.\n'
+             '"CIE Standard Illuminant D Series D65" using "Sharp" CAT.\n'
              '\n\t"XYZ":\n\t\t{0}'.format(XYZ)))
 print(colour.chromatic_adaptation(
     XYZ,
     colour.xy_to_XYZ(A),
-    colour.xy_to_XYZ(D60),
+    colour.xy_to_XYZ(D65),
     method='Von Kries'))
