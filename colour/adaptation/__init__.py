7e1be231d6217d23ad672000fc51531d57795ee8
Thomas Mansencal <thomas.mansencal@gmail.com>
2014-09-10 22:23:22 +0200

    Implement skeleton for "CIE 1994" chromatic adaptation model.

--- colour/adaptation/__init__.py
+++ colour/adaptation/__init__.py
@@ -5,6 +5,8 @@ from __future__ import absolute_import
 
 from .cat import CHROMATIC_ADAPTATION_METHODS
 from .cat import chromatic_adaptation_matrix, chromatic_adaptation
+from .cie1994 import cie1994
 
 __all__ = ['CHROMATIC_ADAPTATION_METHODS']
 __all__ += ['chromatic_adaptation_matrix', 'chromatic_adaptation']
+__all__ += ['cie1994']
