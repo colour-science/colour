4ec13c11e2e5537c883045b1f315d452ba4b0595
Thomas Mansencal <thomas.mansencal@gmail.com>
2014-09-11 19:29:33 +0200

    Implement "CIE 1994" chromatic adaptation model.
    References #120.

--- colour/adaptation/__init__.py
+++ colour/adaptation/__init__.py
@@ -5,8 +5,8 @@ from __future__ import absolute_import
 
 from .cat import CHROMATIC_ADAPTATION_METHODS
 from .cat import chromatic_adaptation_matrix, chromatic_adaptation
-from .cie1994 import cie1994
+from .cie1994 import chromatic_adaptation_cie1994
 
 __all__ = ['CHROMATIC_ADAPTATION_METHODS']
 __all__ += ['chromatic_adaptation_matrix', 'chromatic_adaptation']
-__all__ += ['cie1994']
+__all__ += ['chromatic_adaptation_cie1994']
