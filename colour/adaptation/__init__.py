f1872ac2abcb3013304ba743fae0da7bba15d78b
Thomas Mansencal <thomas.mansencal@gmail.com>
2014-09-13 17:25:16 +0200

    Implement support for "Fairchild (1990)" chromatic adaptation model.
    References #120.

--- colour/adaptation/__init__.py
+++ colour/adaptation/__init__.py
@@ -5,6 +5,7 @@ from __future__ import absolute_import
 
 from .cat import CHROMATIC_ADAPTATION_METHODS
 from .cat import chromatic_adaptation_matrix, chromatic_adaptation
+from .fairchild1990 import chromatic_adaptation_fairchild1990
 from .cmccat2000 import (
     CMCCAT2000_InductionFactors,
     CMCCAT2000_VIEWING_CONDITIONS,
@@ -14,6 +15,7 @@ from .cie1994 import chromatic_adaptation_cie1994
 
 __all__ = ['CHROMATIC_ADAPTATION_METHODS']
 __all__ += ['chromatic_adaptation_matrix', 'chromatic_adaptation']
+__all__ += ['chromatic_adaptation_fairchild1990']
 __all__ += ['CMCCAT2000_InductionFactors',
             'CMCCAT2000_VIEWING_CONDITIONS',
             'CMCCAT2000_forward',
