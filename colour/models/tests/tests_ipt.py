Fix precision loss due to matrices rounding in "IPT" colourspace model.
References #127.

colour/models/tests/tests_ipt.py
@@ -68,18 +68,18 @@ class TestIPT_to_XYZ(unittest.TestCase):
         """
 
         np.testing.assert_almost_equal(
-            IPT_to_XYZ(np.array([0.36571124, -0.11114798, 0.01594746])),
-            np.array([0.07048271, 0.1007966, 0.09558844]),
+            IPT_to_XYZ(np.array([1.00300825, 0.01906918, -0.01369292])),
+            np.array([0.96907232, 1., 1.12179215]),
             decimal=7)
 
         np.testing.assert_almost_equal(
-            IPT_to_XYZ(np.array([0.5916803, 0.34150712, 0.33282621])),
-            np.array([0.47097737, 0.3494794, 0.1130151]),
+            IPT_to_XYZ(np.array([0.73974548, 0.95333412, 1.71951212])),
+            np.array([1.92001986, 1., -0.1241347]),
             decimal=7)
 
         np.testing.assert_almost_equal(
-            IPT_to_XYZ(np.array([0.46626813, 0.25471184, 0.19904068])),
-            np.array([0.25506876, 0.19148927, 0.08849769]),
+            IPT_to_XYZ(np.array([1.06406598, -0.08075812, -0.39625384])),
+            np.array([1.0131677, 1., 2.11217686]),
             decimal=7)
 
 