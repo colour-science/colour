3837671e3af0d5c34bc72a49d7d9864612920a99
Luke Canavan <canavandl@gmail.com>
2014-09-21 14:06:25 -0700

    Update "colour.quality" sub-package imports.

--- colour/quality/__init__.py
+++ colour/quality/__init__.py
@@ -6,7 +6,8 @@ from __future__ import absolute_import
 from .dataset import *  # noqa
 from . import dataset
 from .cri import colour_rendering_index
+from .cqs import colour_quality_scale
 
 __all__ = []
 __all__ += dataset.__all__
-__all__ += ['colour_rendering_index']
+__all__ += ['colour_rendering_index', 'colour_quality_scale']
