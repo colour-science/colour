b47830bc5c5b9d2d4ec332e1eedb6518a0401dcd
Thomas Mansencal <thomas.mansencal@gmail.com>
2014-09-29 19:57:15 +0200

    Ensure "message_box" definition is available at "colour.utilities" package level.

--- colour/utilities/__init__.py
+++ colour/utilities/__init__.py
@@ -5,8 +5,8 @@ from __future__ import absolute_import
 
 from .common import is_scipy_installed, is_string
 from .data_structures import Lookup, Structure, CaseInsensitiveMapping
-from .verbose import warning
+from .verbose import message_box, warning
 
 __all__ = ['is_scipy_installed', 'is_string']
 __all__ += ['Lookup', 'Structure', 'CaseInsensitiveMapping']
-__all__ += ['warning']
+__all__ += ['message_box', 'warning']
