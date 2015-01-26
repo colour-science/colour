e2682c761b442e42611aa4fbc7806c461b5315e9
Thomas Mansencal <thomas.mansencal@gmail.com>
2015-01-26 12:24:11 +0100

    Raise package version to 0.3.4.

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ if sys.version_info[:2] <= (3, 2):
         'mock>=1.0.1']
 
 setup(name='colour-science',
-      version='0.3.3',
+      version='0.3.4',
       author=__author__,
       author_email=__email__,
       include_package_data=True,
