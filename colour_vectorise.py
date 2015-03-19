7c729c0da23cb554de81111677f47ca63ea550ca
Thomas Mansencal <thomas.mansencal@gmail.com>
2015-03-19 21:57:03 +0100

    Add support for "colour.adaptation.fairchild1990" module in vectorisation master module.

--- colour_vectorise.py
+++ colour_vectorise.py
@@ -30,7 +30,6 @@ from colour.utilities import (
     row_as_diagonal,
     warning)
 
-
 # np.random.seed(64)
 
 DATA_HD1 = np.random.rand(1920 * 1080, 3)
@@ -399,6 +398,242 @@ def CMCCAT2000_reverse_analysis():
 
 
 # CMCCAT2000_reverse_analysis()
+
+# #############################################################################
+# #############################################################################
+# ## colour.adaptation.fairchild1990
+# #############################################################################
+# #############################################################################
+from colour.adaptation.fairchild1990 import *
+
+
+def chromatic_adaptation_Fairchild1990_vectorise(XYZ_1,
+                                                 XYZ_n,
+                                                 XYZ_r,
+                                                 Y_n,
+                                                 discount_illuminant=False):
+    """
+    Adapts given *CIE XYZ_1* colourspace stimulus from test viewing conditions
+    to reference viewing conditions using Fairchild (1990) chromatic
+    adaptation model.
+
+    Parameters
+    ----------
+    XYZ_1 : array_like, (3,)
+        *CIE XYZ_1* colourspace matrix of test sample / stimulus in domain
+        [0, 100].
+    XYZ_n : array_like, (3,)
+        Test viewing condition *CIE XYZ_n* colourspace whitepoint matrix.
+    XYZ_r : array_like, (3,)
+        Reference viewing condition *CIE XYZ_r* colourspace whitepoint matrix.
+    Y_n : numeric
+        Luminance :math:`Y_n` of test adapting stimulus in :math:`cd/m^2`.
+    discount_illuminant : bool, optional
+        Truth value indicating if the illuminant should be discounted.
+
+    Returns
+    -------
+    ndarray, (3,)
+        Adapted *CIE XYZ_2* colourspace test stimulus.
+
+    Warning
+    -------
+    The input domain of that definition is non standard!
+
+    Notes
+    -----
+    -   Input *CIE XYZ_1*, *CIE XYZ_n* and *CIE XYZ_r* colourspace matrices are
+        in domain [0, 100].
+    -   Output *CIE XYZ_2* colourspace matrix is in domain [0, 100].
+
+    Examples
+    --------
+    >>> XYZ_1 = np.array([19.53, 23.07, 24.97])
+    >>> XYZ_n = np.array([111.15, 100.00, 35.20])
+    >>> XYZ_r = np.array([94.81, 100.00, 107.30])
+    >>> Y_n = 200
+    >>> chromatic_adaptation_Fairchild1990(XYZ_1, XYZ_n, XYZ_r, Y_n)  # noqa  # doctest: +ELLIPSIS
+    array([ 23.3252634...,  23.3245581...,  76.1159375...])
+    """
+
+    XYZ_1 = np.asarray(XYZ_1)
+    XYZ_n = np.asarray(XYZ_n)
+    XYZ_r = np.asarray(XYZ_r)
+
+    LMS_1 = np.einsum('...ij,...j->...i',
+                      FAIRCHILD1990_XYZ_TO_RGB_MATRIX,
+                      XYZ_1)
+    LMS_n = np.einsum('...ij,...j->...i',
+                      FAIRCHILD1990_XYZ_TO_RGB_MATRIX,
+                      XYZ_n)
+    LMS_r = np.einsum('...ij,...j->...i',
+                      FAIRCHILD1990_XYZ_TO_RGB_MATRIX,
+                      XYZ_r)
+
+    p_LMS = degrees_of_adaptation_vectorise(LMS_1,
+                                            Y_n,
+                                            discount_illuminant=discount_illuminant)
+
+    a_LMS_1 = p_LMS / LMS_n
+    a_LMS_2 = p_LMS / LMS_r
+
+    A_1 = row_as_diagonal(a_LMS_1)
+    A_2 = row_as_diagonal(a_LMS_2)
+
+    LMSp_1 = np.einsum('...ij,...j->...i', A_1, LMS_1)
+
+    c = 0.219 - 0.0784 * np.log10(Y_n)
+    C = row_as_diagonal(tstack((c, c, c)))
+
+    LMS_a = np.einsum('...ij,...j->...i', C, LMSp_1)
+    LMSp_2 = np.einsum('...ij,...j->...i', np.linalg.inv(C), LMS_a)
+
+    LMS_c = np.einsum('...ij,...j->...i', np.linalg.inv(A_2), LMSp_2)
+    XYZ_c = np.einsum('...ij,...j->...i',
+                      FAIRCHILD1990_RGB_TO_XYZ_MATRIX, LMS_c)
+
+    return XYZ_c
+
+
+def XYZ_to_RGB_fairchild1990_vectorise(XYZ):
+    """
+    Converts from *CIE XYZ* colourspace to cone responses.
+
+    Parameters
+    ----------
+    XYZ : array_like, (3,)
+        *CIE XYZ* colourspace matrix.
+
+    Returns
+    -------
+    ndarray, (3,)
+        Cone responses.
+
+    Examples
+    --------
+    >>> XYZ = np.array([19.53, 23.07, 24.97])
+    >>> XYZ_to_RGB_fairchild1990(XYZ)  # doctest: +ELLIPSIS
+    array([ 22.1231935...,  23.6054224...,  22.9279534...])
+    """
+
+    return np.einsum('...ij,...j->...i', FAIRCHILD1990_XYZ_TO_RGB_MATRIX, XYZ)
+
+
+def RGB_to_XYZ_fairchild1990_vectorise(RGB):
+    """
+    Converts from cone responses to *CIE XYZ* colourspace.
+
+    Parameters
+    ----------
+    RGB : array_like, (3,)
+        Cone responses.
+
+    Returns
+    -------
+    ndarray, (3,)
+        *CIE XYZ* colourspace matrix.
+
+    Examples
+    --------
+    >>> RGB = np.array([22.1231935, 23.6054224, 22.9279534])
+    >>> RGB_to_XYZ_fairchild1990(RGB)  # doctest: +ELLIPSIS
+    array([ 19.53,  23.07,  24.97])
+    """
+
+    return np.einsum('...ij,...j->...i', FAIRCHILD1990_RGB_TO_XYZ_MATRIX, RGB)
+
+
+def degrees_of_adaptation_vectorise(LMS, Y_n, v=1 / 3,
+                                    discount_illuminant=False):
+    """
+    Computes the degrees of adaptation :math:`p_L`, :math:`p_M` and
+    :math:`p_S`.
+
+    Parameters
+    ----------
+    LMS : array_like, (3,)
+        Cone responses.
+    Y_n : numeric
+        Luminance :math:`Y_n` of test adapting stimulus in :math:`cd/m^2`.
+    v : numeric, optional
+        Exponent :math:`v`.
+    discount_illuminant : bool, optional
+        Truth value indicating if the illuminant should be discounted.
+
+    Returns
+    -------
+    ndarray, (3,)
+        Degrees of adaptation :math:`p_L`, :math:`p_M` and :math:`p_S`.
+
+    Examples
+    --------
+    >>> LMS = np.array([20.0005206, 19.999783, 19.9988316])
+    >>> Y_n = 31.83
+    >>> degrees_of_adaptation(LMS, Y_n)  # doctest: +ELLIPSIS
+    array([ 0.9799324...,  0.9960035...,  1.0233041...])
+    >>> degrees_of_adaptation(LMS, Y_n, 1 / 3, True)
+    array([1, 1, 1])
+    """
+
+    LMS = np.asarray(LMS)
+    if discount_illuminant:
+        return np.ones(LMS.shape)
+
+    L, M, S = tsplit(LMS)
+
+    LMS_E = np.einsum('...ij,...j->...i',
+                      VON_KRIES_CAT,
+                      np.ones(LMS.shape))  # E illuminant.
+    L_E, M_E, S_E = tsplit(LMS_E)
+
+    Ye_n = Y_n ** v
+
+    f_E = lambda x, y: (3 * (x / y)) / (L / L_E + M / M_E + S / S_E)
+    f_P = lambda x: (1 + Ye_n + x) / (1 + Ye_n + 1 / x)
+
+    p_L = f_P(f_E(L, L_E))
+    p_M = f_P(f_E(M, M_E))
+    p_S = f_P(f_E(S, S_E))
+
+    p_LMS = tstack((p_L, p_M, p_S))
+
+    return p_LMS
+
+
+def chromatic_adaptation_Fairchild1990_analysis():
+    message_box('chromatic_adaptation_Fairchild1990')
+
+    print('Reference:')
+    XYZ_1 = np.array([19.53, 23.07, 24.97])
+    XYZ_n = np.array([111.15, 100.00, 35.20])
+    XYZ_r = np.array([94.81, 100.00, 107.30])
+    Y_n = 200
+    print(chromatic_adaptation_Fairchild1990(XYZ_1, XYZ_n, XYZ_r, Y_n))
+
+    print('\n')
+
+    print('1d array input:')
+    print(chromatic_adaptation_Fairchild1990_vectorise(
+        XYZ_1, XYZ_n, XYZ_r, Y_n))
+
+    print('\n')
+
+    print('2d array input:')
+    XYZ_1 = np.tile(XYZ_1, (6, 1))
+    print(chromatic_adaptation_Fairchild1990_vectorise(
+        XYZ_1, XYZ_n, XYZ_r, Y_n))
+
+    print('\n')
+
+    print('3d array input:')
+    XYZ_1 = np.reshape(XYZ_1, (2, 3, 3))
+    print(chromatic_adaptation_Fairchild1990_vectorise(
+        XYZ_1, XYZ_n, XYZ_r, Y_n))
+
+    print('\n')
+
+
+# chromatic_adaptation_Fairchild1990_analysis()
 
 # #############################################################################
 # #############################################################################
@@ -4301,7 +4536,7 @@ def normalised_primary_matrix_vectorise(primaries, whitepoint):
     z = xy_to_z_vectorise(primaries)[..., np.newaxis]
     primaries = np.transpose(np.hstack((primaries, z)))
 
-    whitepoint = xy_to_XYZ(whitepoint)
+    whitepoint = xy_to_XYZ_vectorise(whitepoint)
 
     coefficients = np.dot(np.linalg.inv(primaries), whitepoint)
     coefficients = np.diagflat(coefficients)
