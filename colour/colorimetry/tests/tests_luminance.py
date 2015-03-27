e427104b9ee0f1dedd69928802b68239cb06a4ed
Thomas Mansencal <thomas.mansencal@gmail.com>
2015-03-27 20:10:58 +0100

    Implement "colour.luminance_Newhall1943", "colour.luminance_ASTMD153508" and "colour.luminance_1976" vectorised definitions.

--- colour/colorimetry/tests/tests_luminance.py
+++ colour/colorimetry/tests/tests_luminance.py
@@ -7,6 +7,7 @@ Defines unit tests for :mod:`colour.colorimetry.luminance` module.
 
 from __future__ import division, unicode_literals
 
+import numpy as np
 import sys
 
 if sys.version_info[:2] <= (2, 6):
@@ -56,6 +57,40 @@ class TestLuminanceNewhall1943(unittest.TestCase):
             2.06998750444,
             places=7)
 
+    def test_n_dimensions_luminance_Newhall1943(self):
+        """
+        Tests :func:`colour.colorimetry.lightness.luminance_Newhall1943`
+        definition n-dimensions support.
+        """
+
+        V = 3.74629715382
+        Y = 10.408987457743208
+        np.testing.assert_almost_equal(
+            luminance_Newhall1943(V),
+            Y,
+            decimal=7)
+
+        V = [V] * 6
+        Y = [Y] * 6
+        np.testing.assert_almost_equal(
+            luminance_Newhall1943(V),
+            Y,
+            decimal=7)
+
+        V = np.reshape(V, (2, 3))
+        Y = np.reshape(Y, (2, 3))
+        np.testing.assert_almost_equal(
+            luminance_Newhall1943(V),
+            Y,
+            decimal=7)
+
+        V = np.reshape(V, (2, 3, 1))
+        Y = np.reshape(Y, (2, 3, 1))
+        np.testing.assert_almost_equal(
+            luminance_Newhall1943(V),
+            Y,
+            decimal=7)
+
 
 class TestLuminanceASTMD153508(unittest.TestCase):
     """
@@ -81,6 +116,40 @@ class TestLuminanceASTMD153508(unittest.TestCase):
             luminance_ASTMD153508(1.52569021578),
             2.01830631474,
             places=7)
+
+    def test_n_dimensions_luminance_ASTMD153508(self):
+        """
+        Tests :func:`colour.colorimetry.lightness.luminance_ASTMD153508`
+        definition n-dimensions support.
+        """
+
+        V = 3.74629715382
+        Y = 10.148809678226682
+        np.testing.assert_almost_equal(
+            luminance_ASTMD153508(V),
+            Y,
+            decimal=7)
+
+        V = [V] * 6
+        Y = [Y] * 6
+        np.testing.assert_almost_equal(
+            luminance_ASTMD153508(V),
+            Y,
+            decimal=7)
+
+        V = np.reshape(V, (2, 3))
+        Y = np.reshape(Y, (2, 3))
+        np.testing.assert_almost_equal(
+            luminance_ASTMD153508(V),
+            Y,
+            decimal=7)
+
+        V = np.reshape(V, (2, 3, 1))
+        Y = np.reshape(Y, (2, 3, 1))
+        np.testing.assert_almost_equal(
+            luminance_ASTMD153508(V),
+            Y,
+            decimal=7)
 
 
 class TestLuminance1976(unittest.TestCase):
@@ -118,6 +187,40 @@ class TestLuminance1976(unittest.TestCase):
             luminance_1976(37.9856290977, 95),
             9.576000000024989,
             places=7)
+
+    def test_n_dimensions_luminance_1976(self):
+        """
+        Tests :func:`colour.colorimetry.lightness.luminance_1976`
+        definition n-dimensions support.
+        """
+
+        Lstar = 37.9856290977
+        Y = 10.080000000026304
+        np.testing.assert_almost_equal(
+            luminance_1976(Lstar),
+            Y,
+            decimal=7)
+
+        Lstar = [Lstar] * 6
+        Y = [Y] * 6
+        np.testing.assert_almost_equal(
+            luminance_1976(Lstar),
+            Y,
+            decimal=7)
+
+        Lstar = np.reshape(Lstar, (2, 3))
+        Y = np.reshape(Y, (2, 3))
+        np.testing.assert_almost_equal(
+            luminance_1976(Lstar),
+            Y,
+            decimal=7)
+
+        Lstar = np.reshape(Lstar, (2, 3, 1))
+        Y = np.reshape(Y, (2, 3, 1))
+        np.testing.assert_almost_equal(
+            luminance_1976(Lstar),
+            Y,
+            decimal=7)
 
 
 if __name__ == '__main__':
