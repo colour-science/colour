46748529352abae71f042daebf54e6fbeac6dd13
Thomas Mansencal <thomas.mansencal@gmail.com>
2015-07-11 22:37:54 +1200

    Add "Typical CRT Brainard 1997" display spectral data.

---
+++ colour/characterisation/dataset/displays/crt/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+from .rgb_primaries import CRT_DISPLAYS_RGB_PRIMARIES
+
+__all__ = ['CRT_DISPLAYS_RGB_PRIMARIES']
