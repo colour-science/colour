856cc9af196575949016b381de95b58424d11783
Thomas Mansencal <thomas.mansencal@gmail.com>
2016-04-15 21:09:29 +1200

    Update incorrect copyright dates in various modules.

--- colour/utilities/tests/tests_data_structures.py
+++ colour/utilities/tests/tests_data_structures.py
@@ -17,7 +17,7 @@ from colour.utilities import (
     CaseInsensitiveMapping)
 
 __author__ = 'Colour Developers'
-__copyright__ = 'Copyright (C) 2008 - 2014 - Colour Developers'
+__copyright__ = 'Copyright (C) 2013 - 2015 - Colour Developers'
 __license__ = 'New BSD License - http://opensource.org/licenses/BSD-3-Clause'
 __maintainer__ = 'Colour Developers'
 __email__ = 'colour-science@googlegroups.com'
