fa1b3b9c158989db308ad02506eaefc23061354c
Nick-Shaw <nick@antlerpost.com>
2016-05-05 17:30:00 +0100

    Add n-dimensional array and nan unit tests.
    Add unit tests for n-dimensional arrays and nan/inf.
    
    Also additional code cleanup and more tests.

--- colour/models/rgb/tests/tests_ycbcr.py
+++ colour/models/rgb/tests/tests_ycbcr.py
@@ -15,6 +15,7 @@ from colour.models.rgb.ycbcr import (RGB_to_YCbCr,
                                      RGB_to_YcCbcCrc,
                                      YcCbcCrc_to_RGB,
                                      YCBCR_WEIGHTS)
+from colour.utilities import ignore_numpy_errors
 
 __author__ = 'Colour Developers'
 __copyright__ = 'Copyright (C) 2013 - 2016 - Colour Developers'
@@ -69,6 +70,50 @@ class TestRGB_to_YCbCr(unittest.TestCase):
             np.array([0.2461898, 0.75392897, 0.79920662]),
             decimal=7)
 
+    def test_n_dimensional_RGB_to_YCbCr(self):
+        """
+        Tests :func:`colour.models.rgb.ycbcr.RGB_to_YCbCr` definition
+        n-dimensional arrays support.
+        """
+        rgb = np.array([0.75, 0.5, 0.25])
+        YCbCr = np.array([0.52230157, 0.36699593, 0.62183309])
+        np.testing.assert_almost_equal(
+            RGB_to_YCbCr(rgb),
+            YCbCr)
+
+        rgb = np.tile(rgb, 4)
+        rgb = np.reshape(rgb, (4,3))
+        YCbCr = np.tile(YCbCr, 4)
+        YCbCr = np.reshape(YCbCr, (4,3))
+        np.testing.assert_almost_equal(
+            RGB_to_YCbCr(rgb),
+            YCbCr)
+
+        rgb = np.tile(rgb, 4)
+        rgb = np.reshape(rgb, (4, 4, 3))
+        YCbCr = np.tile(YCbCr, 4)
+        YCbCr = np.reshape(YCbCr, (4, 4, 3))
+        np.testing.assert_almost_equal(
+            RGB_to_YCbCr(rgb),
+            YCbCr)
+
+        rgb = np.tile(rgb, 4)
+        rgb = np.reshape(rgb, (4, 4, 4, 3))
+        YCbCr = np.tile(YCbCr, 4)
+        YCbCr = np.reshape(YCbCr, (4, 4, 4, 3))
+        np.testing.assert_almost_equal(
+            RGB_to_YCbCr(rgb),
+            YCbCr)
+
+    @ignore_numpy_errors
+    def test_nan_RGB_to_YCbCr(self):
+        """
+        Tests :func:`colour.models.rgb.ycbcr.RGB_to_YCbCr` definition nan
+        support.
+        """
+
+        RGB_to_YCbCr(np.array([-np.inf, np.inf, np.nan]))
+
 
 class TestYCbCr_to_RGB(unittest.TestCase):
     """
@@ -93,6 +138,62 @@ class TestYCbCr_to_RGB(unittest.TestCase):
                          in_int=True),
             np.array([0.25018598, 0.49950072, 0.75040741]),
             decimal=7)
+
+        np.testing.assert_almost_equal(
+            YCbCr_to_RGB(np.array([150, 99, 175]),
+                         in_bits=8,
+                         in_legal=False,
+                         in_int=True,
+                         out_bits=8,
+                         out_legal=True,
+                         out_int=True),
+            np.array([208, 131, 99]),
+            decimal=7)
+
+
+    def test_n_dimensional_YCbCr_to_RGB(self):
+        """
+        Tests :func:`colour.models.rgb.ycbcr.YCbCr_to_RGB` definition
+        n-dimensional arrays support.
+        """
+        YCbCr = np.array([0.52230157, 0.36699593, 0.62183309])
+        rgb = np.array([0.75, 0.5, 0.25])
+        np.testing.assert_almost_equal(
+            YCbCr_to_RGB(YCbCr),
+            rgb)
+
+        rgb = np.tile(rgb, 4)
+        rgb = np.reshape(rgb, (4,3))
+        YCbCr = np.tile(YCbCr, 4)
+        YCbCr = np.reshape(YCbCr, (4,3))
+        np.testing.assert_almost_equal(
+            YCbCr_to_RGB(YCbCr),
+            rgb)
+
+        rgb = np.tile(rgb, 4)
+        rgb = np.reshape(rgb, (4, 4, 3))
+        YCbCr = np.tile(YCbCr, 4)
+        YCbCr = np.reshape(YCbCr, (4, 4, 3))
+        np.testing.assert_almost_equal(
+            YCbCr_to_RGB(YCbCr),
+            rgb)
+
+        rgb = np.tile(rgb, 4)
+        rgb = np.reshape(rgb, (4, 4, 4, 3))
+        YCbCr = np.tile(YCbCr, 4)
+        YCbCr = np.reshape(YCbCr, (4, 4, 4, 3))
+        np.testing.assert_almost_equal(
+            YCbCr_to_RGB(YCbCr),
+            rgb)
+
+    @ignore_numpy_errors
+    def test_nan_YCbCr_to_RGB(self):
+        """
+        Tests :func:`colour.models.rgb.ycbcr.YCbCr_to_RGB` definition nan
+        support.
+        """
+
+        YCbCr_to_RGB(np.array([-np.inf, np.inf, np.nan]))
 
 
 class TestRGB_to_YcCbcCrc(unittest.TestCase):
@@ -120,6 +221,50 @@ class TestRGB_to_YcCbcCrc(unittest.TestCase):
             np.array([422, 512, 512]),
             decimal=7)
 
+    def test_n_dimensional_RGB_to_YcCbcCrc(self):
+        """
+        Tests :func:`colour.models.rgb.ycbcr.RGB_to_YcCbcCrc` definition
+        n-dimensional arrays support.
+        """
+        rgb = np.array([0.75, 0.5, 0.25])
+        YcCbcCrc = np.array([0.69943807, 0.38814348, 0.61264549])
+        np.testing.assert_almost_equal(
+            RGB_to_YcCbcCrc(rgb),
+            YcCbcCrc)
+
+        rgb = np.tile(rgb, 4)
+        rgb = np.reshape(rgb, (4,3))
+        YcCbcCrc = np.tile(YcCbcCrc, 4)
+        YcCbcCrc = np.reshape(YcCbcCrc, (4,3))
+        np.testing.assert_almost_equal(
+            RGB_to_YcCbcCrc(rgb),
+            YcCbcCrc)
+
+        rgb = np.tile(rgb, 4)
+        rgb = np.reshape(rgb, (4, 4, 3))
+        YcCbcCrc = np.tile(YcCbcCrc, 4)
+        YcCbcCrc = np.reshape(YcCbcCrc, (4, 4, 3))
+        np.testing.assert_almost_equal(
+            RGB_to_YcCbcCrc(rgb),
+            YcCbcCrc)
+
+        rgb = np.tile(rgb, 4)
+        rgb = np.reshape(rgb, (4, 4, 4, 3))
+        YcCbcCrc = np.tile(YcCbcCrc, 4)
+        YcCbcCrc = np.reshape(YcCbcCrc, (4, 4, 4, 3))
+        np.testing.assert_almost_equal(
+            RGB_to_YcCbcCrc(rgb),
+            YcCbcCrc)
+
+    @ignore_numpy_errors
+    def test_nan_RGB_to_YcCbcCrc(self):
+        """
+        Tests :func:`colour.models.rgb.ycbcr.RGB_to_YcCbcCrc` definition nan
+        support.
+        """
+
+        RGB_to_YcCbcCrc(np.array([-np.inf, np.inf, np.nan]))
+
 
 class TestYcCbcCrc_to_RGB(unittest.TestCase):
     """
@@ -145,6 +290,50 @@ class TestYcCbcCrc_to_RGB(unittest.TestCase):
             YcCbcCrc_to_RGB(np.array([0.678, 0.4, 0.6])),
             np.array([0.68390184, 0.47285022, 0.25116003]),
             decimal=7)
+
+    def test_n_dimensional_YcCbcCrc_to_RGB(self):
+        """
+        Tests :func:`colour.models.rgb.ycbcr.YcCbcCrc_to_RGB` definition
+        n-dimensional arrays support.
+        """
+        YcCbcCrc = np.array([0.69943807, 0.38814348, 0.61264549])
+        rgb = np.array([0.75, 0.5, 0.25])
+        np.testing.assert_almost_equal(
+            YcCbcCrc_to_RGB(YcCbcCrc),
+            rgb)
+
+        rgb = np.tile(rgb, 4)
+        rgb = np.reshape(rgb, (4,3))
+        YcCbcCrc = np.tile(YcCbcCrc, 4)
+        YcCbcCrc = np.reshape(YcCbcCrc, (4,3))
+        np.testing.assert_almost_equal(
+            YcCbcCrc_to_RGB(YcCbcCrc),
+            rgb)
+
+        rgb = np.tile(rgb, 4)
+        rgb = np.reshape(rgb, (4, 4, 3))
+        YcCbcCrc = np.tile(YcCbcCrc, 4)
+        YcCbcCrc = np.reshape(YcCbcCrc, (4, 4, 3))
+        np.testing.assert_almost_equal(
+            YcCbcCrc_to_RGB(YcCbcCrc),
+            rgb)
+
+        rgb = np.tile(rgb, 4)
+        rgb = np.reshape(rgb, (4, 4, 4, 3))
+        YcCbcCrc = np.tile(YcCbcCrc, 4)
+        YcCbcCrc = np.reshape(YcCbcCrc, (4, 4, 4, 3))
+        np.testing.assert_almost_equal(
+            YcCbcCrc_to_RGB(YcCbcCrc),
+            rgb)
+
+    @ignore_numpy_errors
+    def test_nan_YcCbcCrc_to_RGB(self):
+        """
+        Tests :func:`colour.models.rgb.ycbcr.YcCbcCrc_to_RGB` definition nan
+        support.
+        """
+
+        YcCbcCrc_to_RGB(np.array([-np.inf, np.inf, np.nan]))
 
 
 if __name__ == '__main__':
