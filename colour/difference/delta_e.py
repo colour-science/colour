704c0f0c02a7d083723b6ccb187a5cd21c4cdbbe
Thomas Mansencal <thomas.mansencal@gmail.com>
2016-05-28 11:15:56 +1200

    Add usage notes to "colour.delta_E_CIE1994" and "colour.delta_E_CIE2000" definitions.
    References #268.

--- colour/difference/delta_e.py
+++ colour/difference/delta_e.py
@@ -112,6 +112,13 @@ def delta_E_CIE1994(Lab_1, Lab_2, textiles=True, **kwargs):
     numeric or ndarray
         Colour difference :math:`\Delta E_{ab}`.
 
+    Notes
+    -----
+    CIE 1994 colour differences are not symmetrical: difference between
+    `Lab_1` and `Lab_2` may not be the same as difference between `Lab_2` and
+    `Lab_1` thus one colour must be understood to be the reference against
+    which a sample colour is compared.
+
     References
     ----------
     .. [3]  Lindbloom, B. (2011). Delta E (CIE 1994). Retrieved February 24,
@@ -178,6 +185,13 @@ def delta_E_CIE2000(Lab_1, Lab_2, **kwargs):
     -------
     numeric or ndarray
         Colour difference :math:`\Delta E_{ab}`.
+
+    Notes
+    -----
+    CIE 2000 colour differences are not symmetrical: difference between
+    `Lab_1` and `Lab_2` may not be the same as difference between `Lab_2` and
+    `Lab_1` thus one colour must be understood to be the reference against
+    which a sample colour is compared.
 
     References
     ----------
