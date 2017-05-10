a6b725dbc1a71fceb114cebfec86159e88aee8f6
Thomas Mansencal <thomas.mansencal@gmail.com>
2017-05-10 23:33:22 +1200

    Implement support for "Meng et al. (2015)" reflectance recovery.
    Closes #182.

--- colour/recovery/__init__.py
+++ colour/recovery/__init__.py
@@ -5,8 +5,10 @@ from __future__ import absolute_import
 
 from .dataset import *  # noqa
 from . import dataset
+from .meng2015 import XYZ_to_spectral_Meng2015
 from .smits1999 import RGB_to_spectral_Smits1999
 
 __all__ = []
 __all__ += dataset.__all__
+__all__ += ['XYZ_to_spectral_Meng2015']
 __all__ += ['RGB_to_spectral_Smits1999']
