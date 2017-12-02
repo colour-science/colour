afee6b810b0fcf66fed654e4bd0ef53ff40c9df7
Thomas Mansencal <thomas.mansencal@gmail.com>
2017-12-02 18:21:18 +1300

    Add "colour.delta_E_Luo2006" wrapper definition and "colour.DELTA_E_LUO2006_METHODS" attribute.

--- colour/difference/__init__.py
+++ colour/difference/__init__.py
@@ -5,11 +5,15 @@ from __future__ import absolute_import
 
 from .delta_e import (DELTA_E_METHODS, delta_E, delta_E_CIE1976,
                       delta_E_CIE1994, delta_E_CIE2000, delta_E_CMC)
-from .delta_e_luo2006 import (delta_E_CAM02LCD, delta_E_CAM02SCD,
-                              delta_E_CAM02UCS)
+from .delta_e_luo2006 import (DELTA_E_LUO2006_METHODS, delta_E_CAM02LCD,
+                              delta_E_CAM02SCD, delta_E_CAM02UCS,
+                              delta_E_Luo2006)
 
 __all__ = [
     'DELTA_E_METHODS', 'delta_E', 'delta_E_CIE1976', 'delta_E_CIE1994',
     'delta_E_CIE2000', 'delta_E_CMC'
 ]
-__all__ += ['delta_E_CAM02LCD', 'delta_E_CAM02SCD', 'delta_E_CAM02UCS']
+__all__ += [
+    'DELTA_E_LUO2006_METHODS', 'delta_E_CAM02LCD', 'delta_E_CAM02SCD',
+    'delta_E_CAM02UCS', 'delta_E_Luo2006'
+]
--- colour/difference/delta_e_luo2006.py
+++ colour/difference/delta_e_luo2006.py
@@ -7,8 +7,6 @@
 Defines :math:`\Delta E'` colour difference computation objects based on
 *Luo et al. (2006)* *CAM02-LCD*, *CAM02-SCD*, and *CAM02-UCS* colourspaces:
 
-The following objects are available:
-
 -   :func:`delta_E_CAM02LCD`
 -   :func:`delta_E_CAM02SCD`
 -   :func:`delta_E_CAM02UCS`
@@ -30,7 +28,7 @@ from __future__ import division, unicode_literals
 
 import numpy as np
 
-from colour.utilities import tsplit
+from colour.utilities import CaseInsensitiveMapping, tsplit
 from colour.models.ucs_luo2006 import COEFFICIENTS_UCS_LUO2006
 
 __author__ = 'Colour Developers'
@@ -41,12 +39,12 @@ __email__ = 'colour-science@googlegroups.com'
 __status__ = 'Production'
 
 __all__ = [
-    'delta_E_Luo2006', 'delta_E_CAM02LCD', 'delta_E_CAM02SCD',
-    'delta_E_CAM02UCS'
+    'delta_E_CAM02LCD', 'delta_E_CAM02SCD', 'delta_E_CAM02UCS',
+    'DELTA_E_LUO2006_METHODS', 'delta_E_Luo2006'
 ]
 
 
-def delta_E_Luo2006(Jpapbp_1, Jpapbp_2, coefficients):
+def _delta_E_Luo2006(Jpapbp_1, Jpapbp_2, coefficients):
     """
     Returns the difference :math:`\Delta E'` between two given
     *Luo et al. (2006)* *CAM02-LCD*, *CAM02-SCD*, or *CAM02-UCS* colourspaces
@@ -73,7 +71,7 @@ def delta_E_Luo2006(Jpapbp_1, Jpapbp_2, coefficients):
     --------
     >>> Jpapbp_1 = np.array([54.90433134, -0.08450395, -0.06854831])
     >>> Jpapbp_2 = np.array([54.90433134, -0.08442362, -0.06848314])
-    >>> delta_E_Luo2006(  # doctest: +ELLIPSIS
+    >>> _delta_E_Luo2006(  # doctest: +ELLIPSIS
     ...     Jpapbp_1, Jpapbp_2, COEFFICIENTS_UCS_LUO2006['CAM02-LCD'])
     0.0001034...
     """
@@ -113,8 +111,8 @@ def delta_E_CAM02LCD(Jpapbp_1, Jpapbp_2):
     >>> delta_E_CAM02LCD(Jpapbp_1, Jpapbp_2)  # doctest: +ELLIPSIS
     0.0001034...
     """
-    return delta_E_Luo2006(Jpapbp_1, Jpapbp_2,
-                           COEFFICIENTS_UCS_LUO2006['CAM02-LCD'])
+    return _delta_E_Luo2006(Jpapbp_1, Jpapbp_2,
+                            COEFFICIENTS_UCS_LUO2006['CAM02-LCD'])
 
 
 def delta_E_CAM02SCD(Jpapbp_1, Jpapbp_2):
@@ -143,8 +141,8 @@ def delta_E_CAM02SCD(Jpapbp_1, Jpapbp_2):
     >>> delta_E_CAM02SCD(Jpapbp_1, Jpapbp_2)  # doctest: +ELLIPSIS
     0.0001034...
     """
-    return delta_E_Luo2006(Jpapbp_1, Jpapbp_2,
-                           COEFFICIENTS_UCS_LUO2006['CAM02-SCD'])
+    return _delta_E_Luo2006(Jpapbp_1, Jpapbp_2,
+                            COEFFICIENTS_UCS_LUO2006['CAM02-SCD'])
 
 
 def delta_E_CAM02UCS(Jpapbp_1, Jpapbp_2):
@@ -173,5 +171,54 @@ def delta_E_CAM02UCS(Jpapbp_1, Jpapbp_2):
     >>> delta_E_CAM02UCS(Jpapbp_1, Jpapbp_2)  # doctest: +ELLIPSIS
     0.0001034...
     """
-    return delta_E_Luo2006(Jpapbp_1, Jpapbp_2,
-                           COEFFICIENTS_UCS_LUO2006['CAM02-UCS'])
+    return _delta_E_Luo2006(Jpapbp_1, Jpapbp_2,
+                            COEFFICIENTS_UCS_LUO2006['CAM02-UCS'])
+
+
+DELTA_E_LUO2006_METHODS = CaseInsensitiveMapping({
+    'CAM02-LCD': delta_E_CAM02LCD,
+    'CAM02-SCD': delta_E_CAM02SCD,
+    'CAM02-UCS': delta_E_CAM02UCS
+})
+"""
+Supported :math:`\Delta E_{ab}` *Luo et al. (2006)* computations methods.
+
+DELTA_E_METHODS : CaseInsensitiveMapping
+    **{'CAM02-LCD', 'CAM02-SCD', 'CAM02-UCS'}**
+"""
+
+
+def delta_E_Luo2006(Jpapbp_1, Jpapbp_2, method='CAM02-UCS'):
+    """
+    Returns the difference :math:`\Delta E'` between two given
+    *Luo et al. (2006)* *CAM02-LCD*, *CAM02-SCD*, or *CAM02-UCS* colourspaces
+    :math:`J'a'b'` arrays using given method.
+
+    Parameters
+    ----------
+    Jpapbp_1 : array_like
+        Standard / reference *Luo et al.* (2006) *CAM02-LCD*, *CAM02-SCD*, or
+        *CAM02-UCS* colourspaces :math:`J'a'b'` array.
+    Jpapbp_2 : array_like
+        Sample / test *Luo et al. (2006)* *CAM02-LCD*, *CAM02-SCD*, or
+        *CAM02-UCS* colourspaces :math:`J'a'b'` array.
+    method : unicode, optional
+        **{'CAM02-LCD', 'CAM02-SCD', 'CAM02-UCS'}**,
+        Computation method.
+
+    Returns
+    -------
+    numeric or ndarray
+        Colour difference :math:`\Delta E'`.
+
+    Examples
+    --------
+    >>> Jpapbp_1 = np.array([54.90433134, -0.08450395, -0.06854831])
+    >>> Jpapbp_2 = np.array([54.90433134, -0.08442362, -0.06848314])
+    >>> delta_E_Luo2006(Jpapbp_1, Jpapbp_2)  # doctest: +ELLIPSIS
+    0.0001034...
+    >>> delta_E_Luo2006(Jpapbp_1, Jpapbp_2, 'CAM02-LCD')  # doctest: +ELLIPSIS
+    0.0001034...
+    """
+
+    return DELTA_E_LUO2006_METHODS.get(method)(Jpapbp_1, Jpapbp_2)
