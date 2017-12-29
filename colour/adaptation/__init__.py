7fc0f7288259225657e5d6de9c22734d410f182b
Thomas Mansencal <thomas.mansencal@gmail.com>
2017-12-29 19:57:03 +1300

    Implement "colour.chromatic_adaptation" definition and "colour.CHROMATIC_ADAPTATION_METHODS" attribute.

--- colour/adaptation/__init__.py
+++ colour/adaptation/__init__.py
@@ -3,6 +3,10 @@
 
 from __future__ import absolute_import
 
+import numpy as np
+
+from colour.utilities import CaseInsensitiveMapping, filter_kwargs
+
 from .dataset import *  # noqa
 from . import dataset
 from .vonkries import (chromatic_adaptation_matrix_VonKries,
@@ -27,3 +31,163 @@ __all__ += [
     'chromatic_adaptation_CMCCAT2000'
 ]
 __all__ += ['chromatic_adaptation_CIE1994']
+
+CHROMATIC_ADAPTATION_METHODS = CaseInsensitiveMapping({
+    'CIE 1994': chromatic_adaptation_CIE1994,
+    'CMCCAT2000': chromatic_adaptation_CMCCAT2000,
+    'Fairchild 1990': chromatic_adaptation_Fairchild1990,
+    'Von Kries': chromatic_adaptation_VonKries,
+})
+"""
+Supported chromatic adaptation methods.
+
+CHROMATIC_ADAPTATION_METHODS : CaseInsensitiveMapping
+    **{'CIE 1994', 'CMCCAT2000', 'Fairchild 1990', 'Von Kries'}**
+"""
+
+
+def chromatic_adaptation(XYZ, XYZ_w, XYZ_wr, method='Von Kries', **kwargs):
+    """
+    Adapts given stimulus from test viewing conditions to reference viewing
+    conditions.
+
+    Parameters
+    ----------
+    XYZ : array_like
+        *CIE XYZ* tristimulus values of stimulus to adapt.
+    XYZ_w : array_like
+        Test viewing condition *CIE XYZ* tristimulus values of the whitepoint.
+    XYZ_wr : array_like
+        Reference viewing condition *CIE XYZ* tristimulus values of the
+        whitepoint.
+    method : unicode, optional
+        **{'Von Kries', 'CIE 1994', 'CMCCAT2000', 'Fairchild 1990'}**,
+        Computation method.
+
+    Other Parameters
+    ----------------
+    E_o1 : numeric
+        {:func:`chromatic_adaptation_CIE1994`},
+        Test illuminance :math:`E_{o1}` in :math:`cd/m^2`.
+    E_o2 : numeric
+        {:func:`chromatic_adaptation_CIE1994`},
+        Reference illuminance :math:`E_{o2}` in :math:`cd/m^2`.
+    L_A1 : numeric or array_like
+        {:func:`chromatic_adaptation_CMCCAT2000`},
+        Luminance of test adapting field :math:`L_{A1}` in :math:`cd/m^2`.
+    L_A2 : numeric or array_like
+        {:func:`chromatic_adaptation_CMCCAT2000`},
+        Luminance of reference adapting field :math:`L_{A2}` in :math:`cd/m^2`.
+    Y_n : numeric or array_like
+        {:func:`chromatic_adaptation_Fairchild1990`},
+        Luminance :math:`Y_n` of test adapting stimulus in :math:`cd/m^2`.
+    Y_o : numeric
+        {:func:`chromatic_adaptation_CIE1994`},
+        Luminance factor :math:`Y_o` of achromatic background as percentage in
+        domain [18, 100].
+    direction : unicode, optional
+        {:func:`chromatic_adaptation_CMCCAT2000`},
+        **{'Forward', 'Reverse'}**,
+        Chromatic adaptation direction.
+    discount_illuminant : bool, optional
+        {:func:`chromatic_adaptation_Fairchild1990`},
+        Truth value indicating if the illuminant should be discounted.
+    n : numeric, optional
+        {:func:`chromatic_adaptation_CIE1994`},
+        Noise component in fundamental primary system.
+    surround : CMCCAT2000_InductionFactors, optional
+        {:func:`chromatic_adaptation_CMCCAT2000`},
+        Surround viewing conditions induction factors.
+    transform : unicode, optional
+        {:func:`chromatic_adaptation_VonKries`},
+        **{'CAT02', 'XYZ Scaling', 'Von Kries', 'Bradford', 'Sharp',
+        'Fairchild', 'CMCCAT97', 'CMCCAT2000', 'CAT02_BRILL_CAT', 'Bianco',
+        'Bianco PC'}**,
+        Chromatic adaptation transform.
+
+    Returns
+    -------
+    ndarray
+        *CIE XYZ_c* tristimulus values of the stimulus corresponding colour.
+
+    Examples
+    --------
+
+    *Von Kries* chromatic adaptation:
+
+    >>> import numpy as np
+    >>> XYZ = np.array([0.07049534, 0.10080000, 0.09558313])
+    >>> XYZ_w = np.array([1.09846607, 1.00000000, 0.35582280])
+    >>> XYZ_wr = np.array([0.95042855, 1.00000000, 1.08890037])
+    >>> chromatic_adaptation(XYZ, XYZ_w, XYZ_wr)
+    ... # doctest: +ELLIPSIS
+    array([ 0.0839746...,  0.1141321...,  0.2862554...])
+
+    *CIE 1994* chromatic adaptation, requires extra *kwargs*:
+
+    >>> XYZ = np.array([0.2800, 0.2126, 0.0527])
+    >>> XYZ_w = np.array([1.09867452, 1.00000000, 0.35591556])
+    >>> XYZ_wr = np.array([0.95045593, 1.00000000, 1.08905775])
+    >>> Y_o = 20
+    >>> E_o = 1000
+    >>> chromatic_adaptation(
+    ...     XYZ, XYZ_w, XYZ_wr, method='CIE 1994', Y_o=Y_o, E_o1=E_o, E_o2=E_o)
+    ... # doctest: +ELLIPSIS
+    array([ 0.2403379...,  0.2115621...,  0.1764301...])
+
+    *CMCCAT2000* chromatic adaptation, requires extra *kwargs*:
+
+    >>> XYZ = np.array([0.2248, 0.2274, 0.0854])
+    >>> XYZ_w = np.array([1.1115, 1.0000, 0.3520])
+    >>> XYZ_wr = np.array([0.9481, 1.0000, 1.0730])
+    >>> L_A = 200
+    >>> chromatic_adaptation(
+    ...     XYZ, XYZ_w, XYZ_wr, method='CMCCAT2000', L_A1=L_A, L_A2=L_A)
+    ... # doctest: +ELLIPSIS
+    array([ 0.1952698...,  0.2306834...,  0.2497175...])
+
+    *Fairchild (1990)* chromatic adaptation, requires extra *kwargs*:
+
+    >>> XYZ = np.array([0.1953, 0.2307, 0.2497])
+    >>> Y_n = 200
+    >>> chromatic_adaptation(
+    ...     XYZ, XYZ_w, XYZ_wr, method='Fairchild 1990', Y_n=Y_n)
+    ... # doctest: +ELLIPSIS
+    array([ 0.2332526...,  0.2332455...,  0.7611593...])
+    """
+
+    XYZ = np.asarray(XYZ)
+    XYZ_w = np.asarray(XYZ_w)
+    XYZ_wr = np.asarray(XYZ_wr)
+
+    function = CHROMATIC_ADAPTATION_METHODS[method]
+
+    # Callables with percentage domain.
+    # TODO: Handle scaling with metadata.
+    percentage_domain = (chromatic_adaptation_CIE1994,
+                         chromatic_adaptation_CMCCAT2000,
+                         chromatic_adaptation_Fairchild1990)
+
+    if function in percentage_domain:
+        XYZ = XYZ * 100
+        XYZ_w = XYZ_w * 100
+        XYZ_wr = XYZ_wr * 100
+
+    kwargs.update({'XYZ_w': XYZ_w, 'XYZ_wr': XYZ_wr})
+
+    if function is chromatic_adaptation_CIE1994:
+        from colour.models import XYZ_to_xy
+
+        kwargs.update({'xy_o1': XYZ_to_xy(XYZ_w), 'xy_o2': XYZ_to_xy(XYZ_wr)})
+    elif function is chromatic_adaptation_Fairchild1990:
+        kwargs.update({'XYZ_n': XYZ_w, 'XYZ_r': XYZ_wr})
+
+    XYZ_c = function(XYZ, **filter_kwargs(function, **kwargs))
+
+    if function in percentage_domain:
+        XYZ_c /= 100
+
+    return XYZ_c
+
+
+__all__ += ['CHROMATIC_ADAPTATION_METHODS', 'chromatic_adaptation']
