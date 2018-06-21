f1930d28658089216c8d204a0cee3769ae4f46b2
Thomas Mansencal <thomas.mansencal@gmail.com>
2018-06-21 18:41:26 +1200

    Slightly increase bars width in "colour.plotting.single_spd_plot" definition.

--- colour/plotting/colorimetry.py
+++ colour/plotting/colorimetry.py
@@ -144,10 +144,12 @@ def single_spd_plot(spd,
         facecolor='none',
         edgecolor='none')
     axes.add_patch(polygon)
+
+    padding = 0.1
     axes.bar(
-        x=wavelengths,
+        x=wavelengths - padding,
         height=max(values),
-        width=1.025,
+        width=1 + padding,
         color=colours,
         align='edge',
         clip_path=polygon)
@@ -230,8 +232,7 @@ def multi_spd_plot(spds,
 
     cmfs = get_cmfs(cmfs)
 
-    illuminant = ILLUMINANTS_SPDS[
-        DEFAULT_PLOTTING_COLOURSPACE.illuminant]
+    illuminant = ILLUMINANTS_SPDS[DEFAULT_PLOTTING_COLOURSPACE.illuminant]
 
     x_limit_min, x_limit_max, y_limit_min, y_limit_max = [], [], [], []
     for spd in spds:
@@ -496,8 +497,8 @@ def multi_illuminant_relative_spd_plot(illuminants=None, **kwargs):
 
     settings = {
         'title':
-            '{0} - Illuminants Relative Spectral Power Distribution'
-            .format(', '.join([spd.strict_name for spd in spds])),
+            '{0} - Illuminants Relative Spectral Power Distribution'.format(
+                ', '.join([spd.strict_name for spd in spds])),
         'y_label':
             'Relative Power'
     }
@@ -759,9 +760,10 @@ def blackbody_spectral_radiance_plot(
     return render(**settings)
 
 
-def blackbody_colours_plot(shape=SpectralShape(150, 12500, 50),
-                           cmfs='CIE 1931 2 Degree Standard Observer',
-                           **kwargs):
+def blackbody_colours_plot(
+        shape=SpectralShape(150, 12500, 50),
+        cmfs='CIE 1931 2 Degree Standard Observer',
+        **kwargs):
     """
     Plots blackbody colours.
 
