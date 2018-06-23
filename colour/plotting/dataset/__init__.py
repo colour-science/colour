9642b5c7626e0f8c556c54426d089dff2dd9d937
Thomas Mansencal <thomas.mansencal@gmail.com>
2018-06-23 15:55:20 +1200

    Update "ASTM G-173" dataset.

--- colour/plotting/dataset/__init__.py
+++ colour/plotting/dataset/__init__.py
@@ -2,6 +2,9 @@
 
 from __future__ import absolute_import
 
-from .astm_g_173 import ASTM_G_173_ETR
+from .astm_g_173 import (ASTM_G_173_ETR, ASTM_G_173_GLOBAL_TILT,
+                         ASTM_G_173_DIRECT_CIRCUMSOLAR)
 
-__all__ = ['ASTM_G_173_ETR']
+__all__ = [
+    'ASTM_G_173_ETR', 'ASTM_G_173_GLOBAL_TILT', 'ASTM_G_173_DIRECT_CIRCUMSOLAR'
+]
