64333d63533c8738829a537da65f261c59f60847
Thomas Mansencal <thomas.mansencal@gmail.com>
2018-10-13 15:27:17 +1300

    Update various references.

--- colour/models/rgb/dataset/aces.py
+++ colour/models/rgb/dataset/aces.py
@@ -34,13 +34,13 @@ References
 -   :cite:`TheAcademyofMotionPictureArtsandSciences2014s` : The Academy of
     Motion Picture Arts and Sciences, Science and Technology Council, &
     Academy Color Encoding System (ACES) Project Subcommittee. (2014).
-    Specification S-2013-001 - ACESproxy , an Integer Log Encoding of ACES
+    Specification S-2013-001 - ACESproxy, an Integer Log Encoding of ACES
     Image Data. Retrieved from
     https://github.com/ampas/aces-dev/tree/master/documents
 -   :cite:`TheAcademyofMotionPictureArtsandSciences2014t` : The Academy of
     Motion Picture Arts and Sciences, Science and Technology Council, & Academy
     Color Encoding System (ACES) Project Subcommittee. (2014). Specification
-    S-2014-003 - ACEScc , A Logarithmic Encoding of ACES Data for use within
+    S-2014-003 - ACEScc, A Logarithmic Encoding of ACES Data for use within
     Color Grading Systems. Retrieved from
     https://github.com/ampas/aces-dev/tree/master/documents
 -   :cite:`TheAcademyofMotionPictureArtsandSciences2015b` : The Academy of
