4eac8ebce4a570ec862f3d32dde330be16f7488e
Thomas Mansencal <thomas.mansencal@gmail.com>
2019-03-20 21:13:58 +1300

    Update various examples.

--- colour/examples/models/examples_ycocg.py
+++ colour/examples/models/examples_ycocg.py
@@ -10,7 +10,7 @@ from colour.utilities import message_box
 
 message_box('"YCoCg" Colour Encoding Computations')
 
-RGB = np.array([0.75, 0.75, 0.0])
+RGB = np.array([0.45620519, 0.03081071, 0.04091952])
 message_box(('Converting to "YCoCg" colour encoding from given "RGB" '
              'colourspace values:\n'
              '\n\t{0}'.format(RGB)))
@@ -18,7 +18,7 @@ print(colour.RGB_to_YCoCg(RGB))
 
 print('\n')
 
-YCoCg = np.array([0.5625, 0.375, 0.1875])
+YCoCg = np.array([0.13968653, 0.20764283, -0.10887582])
 message_box(
     ('Converting to "RGB" colourspace values from "YCoCg" colour encoding '
      'values:\n'
