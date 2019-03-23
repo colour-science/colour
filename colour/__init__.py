936522e595c428f615a1f6cc7b775cddd1d2ca52
Thomas Mansencal <thomas.mansencal@gmail.com>
2019-03-24 12:09:30 +1300

    Raise package version to 0.3.12.

--- colour/__init__.py
+++ colour/__init__.py
@@ -283,7 +283,7 @@ __application_name__ = 'Colour'
 
 __major_version__ = '0'
 __minor_version__ = '3'
-__change_version__ = '11'
+__change_version__ = '12'
 __version__ = '.'.join(
     (__major_version__,
      __minor_version__,
