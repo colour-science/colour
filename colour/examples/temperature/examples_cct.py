2ccf9a2be337f032ad589dad182f08412ae12ca5
Thomas Mansencal <thomas.mansencal@gmail.com>
2019-05-04 15:36:08 +1200

    Update "examples_cct.py" example module.

--- colour/examples/temperature/examples_cct.py
+++ colour/examples/temperature/examples_cct.py
@@ -63,16 +63,17 @@ print(colour.temperature.CCT_to_uv_Krystek1985(CCT))
 print('\n')
 
 xy = colour.ILLUMINANTS['CIE 1931 2 Degree Standard Observer']['D65']
-message_box(('Converting to "CCT" from given "xy" chromaticity coordinates '
-             'using "McCamy (1992)" method:\n'
+message_box(('Converting to "CCT" from given "CIE xy" chromaticity '
+             'coordinates using "McCamy (1992)" method:\n'
              '\n\t{0}'.format(xy)))
 print(colour.xy_to_CCT(xy, method='McCamy 1992'))
 print(colour.temperature.xy_to_CCT_McCamy1992(xy))
 
 print('\n')
 
-message_box(('Converting to "CCT" from given "xy" chromaticity coordinates '
-             'using "Hernandez-Andres, Lee and Romero (1999)" method:\n'
+message_box(('Converting to "CCT" from given "CIE xy" chromaticity '
+             'coordinates using "Hernandez-Andres, Lee and Romero (1999)" '
+             'method:\n'
              '\n\t{0}'.format(xy)))
 print(colour.xy_to_CCT(xy, method='Hernandez 1999'))
 print(colour.temperature.xy_to_CCT_Hernandez1999(xy))
@@ -80,16 +81,17 @@ print(colour.temperature.xy_to_CCT_Hernandez1999(xy))
 print('\n')
 
 CCT = 6503.49254150
-message_box(('Converting to "xy" chromaticity coordinates from given "CCT" '
-             'using "Kang, Moon, Hong, Lee, Cho and Kim (2002)" method:\n'
+message_box(('Converting to "CIE xy" chromaticity coordinates from given '
+             '"CCT" using "Kang, Moon, Hong, Lee, Cho and Kim (2002)" '
+             'method:\n'
              '\n\t{0}'.format(CCT)))
 print(colour.CCT_to_xy(CCT, method='Kang 2002'))
 print(colour.temperature.CCT_to_xy_Kang2002(CCT))
 
 print('\n')
 
-message_box(('Converting to "xy" chromaticity coordinates from given "CCT" '
-             'using "CIE Illuminant D Series" method:\n'
+message_box(('Converting to "CIE xy" chromaticity coordinates from given '
+             '"CCT" using "CIE Illuminant D Series" method:\n'
              '\n\t{0}'.format(CCT)))
 print(colour.CCT_to_xy(CCT, method='CIE Illuminant D Series'))
 print(colour.temperature.CCT_to_xy_CIE_D(CCT))
