b7a33e64553dca9f563e2c38ebae0b9840b336c1
Zach Lewis <zachcanbereached@gmail.com>
2019-09-21 11:16:40 -0400

    add rez package

---
+++ package.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+name = u'colour'
+
+version = '0.3.14'
+
+description = u'Colour Science for Python'
+
+requires = [
+    'imageio-2.0.0+',
+    'six-1.10.0+',
+    'scipy-0.16.0+'
+]
+
+variants = [
+    ['platform-osx', 'arch-x86_64', 'os-osx-10.14.5', 'python-2.7'],
+]
+
+def commands():
+    env.PYTHONPATH.append('{root}/python')
+
+timestamp = 1562044253
+
+format_version = 2
+
+hashed_variants = True
+
+is_pure_python = True
+
+#from_pip = True
+
+pip_name = u'colour-science (0.3.14)'
