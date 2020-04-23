a1b7a9804f36cc97304e650c5ed491165123a68a
Thomas Mansencal <thomas.mansencal@gmail.com>
2020-04-23 18:12:20 +1200

    Reference "NumFOCUS" directly in the code.

--- colour/__init__.py
+++ colour/__init__.py
@@ -10,6 +10,10 @@ of algorithms and datasets for colour science.
 It is freely available under the
 `New BSD License <https://opensource.org/licenses/BSD-3-Clause>`__ terms.
 
+`Colour <https://github.com/colour-science/colour>`__ is an affiliated project
+of `NumFOCUS <https://numfocus.org/>`__, a 501(c)(3) nonprofit in the United
+States.
+
 Sub-packages
 ------------
 -   adaptation: Chromatic adaptation models and transformations.
