334697d56ad0494a9a4b43cdbc7ce70f0193af1f
Zach Lewis <zachcanbereached@gmail.com>
2020-08-25 18:51:32 -0400

    rez package ver uptick

--- package.py
+++ package.py
@@ -1,29 +1,51 @@
 # -*- coding: utf-8 -*-
 
-name = u'colour'
+name = 'colour'
 
-version = '0.3.14.z1'
+version = '0.3.16.0.dev082501'
 
-description = u'Colour Science for Python'
+description = 'Colour Science for Python'
+
+authors = ["Colour Developers"]
+
+help = 'http://colour-science.org'
+
+license = 'BSD-3-Clause'
 
 requires = [
+    '~python-2.7|3.5+',
     'imageio-2.0.0+',
     'six-1.10.0+',
     'scipy-0.16.0+',
-    'networkx',
-    'pygraphviz',
+    'networkx-2.2+<3',
+    'pygraphviz-1.5+',
+    'matplotlib-2+<4',
+    'openimageio-1+<3',
+    'numpy',
+]
+
+private_build_requires = [
+    'setuptools',
+    'pip',
 ]
 
 variants = [
-    ['platform-osx', 'arch-x86_64', 'os-osx-10.14.5', 'python-2.7'],
+    ['python-2.7'],
+    ['python-3.7'],
 ]
 
-def commands():
-    env.PYTHONPATH.append('{root}')
 
-timestamp = 1562044253
+def pre_build_commands():
+    env.PYTHONUSERBASE = '{build.install_path}'
+
+
+build_command = "pip install {root} --no-deps --user --no-compile --global-option='build_ext'"
+
+
+def commands():
+    pyver = '.'.join(
+        map(str, [resolve.python.version.major, resolve.python.version.minor]))
+    env.PYTHONPATH.prepend('{root}/lib/python{pyver}/site-packages')
+
 
 format_version = 2
-
-hashed_variants = True
-
