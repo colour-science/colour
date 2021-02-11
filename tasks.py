64c7ebc3543a840994f4e96bb5f18f6c032de34e
SGeetansh <saxenageetansh1@gmail.com>
2021-02-12 02:28:05 +0530

    Added --traverse-namespace to nosetest

--- tasks.py
+++ tasks.py
@@ -147,8 +147,9 @@ def tests(ctx, nose=True):
     if nose:
         message_box('Running "Nosetests"...')
         ctx.run(
-            'nosetests --with-doctest --with-coverage --cover-package={0} {0}'.
-            format(PYTHON_PACKAGE_NAME),
+            ("nosetests --with-doctest --with-coverage "
+             "--traverse-namespace --cover-package={0} {0}"
+             ).format(PYTHON_PACKAGE_NAME),
             env={'MPLBACKEND': 'AGG'})
     else:
         message_box('Running "Pytest"...')
