bbddd076b45bebd9b553ddbc2c1426ae12a311cd
Thomas Mansencal <thomas.mansencal@gmail.com>
2021-03-07 22:13:51 +1300

    Update "tasks.py" file.

--- tasks.py
+++ tasks.py
@@ -68,6 +68,7 @@ def clean(ctx, docs=True, bytecode=False):
         patterns.append('docs/generated')
 
     if bytecode:
+        patterns.append('**/__pycache__')
         patterns.append('**/*.pyc')
 
     for pattern in patterns:
@@ -318,8 +319,8 @@ def requirements(ctx):
     """
 
     message_box('Exporting "requirements.txt" file...')
-    ctx.run('poetry run pip freeze | '
-            'egrep -v "github.com/colour-science|enum34" '
+    ctx.run('poetry run pip list --format=freeze | '
+            'egrep -v "github.com/colour-science" '
             '> requirements.txt')
 
 
