1ab4bcc29dd634bf19859a8d6d9a174974dff553
Shivank Chaudhary <81817735+Alt-Shivam@users.noreply.github.com>
2022-01-30 01:00:28 +0530

    Added support for Rayleigh-Jeans spectral distribution
    (cherry picked from commit 285fef1b3cbab66064244ef92259de7bf5122783)

--- colour/colorimetry/__init__.py
+++ colour/colorimetry/__init__.py
@@ -8,7 +8,13 @@ from .spectrum import (
     sds_and_msds_to_sds,
     sds_and_msds_to_msds,
 )
-from .blackbody import sd_blackbody, blackbody_spectral_radiance, planck_law
+from .blackbody import (
+    sd_blackbody,
+    blackbody_spectral_radiance,
+    planck_law,
+    rayleigh_jeans_law,
+    sd_rayleigh_jeans,
+)
 from .cmfs import (
     LMS_ConeFundamentals,
     RGB_ColourMatchingFunctions,
@@ -133,6 +139,8 @@ __all__ += [
     "sd_blackbody",
     "blackbody_spectral_radiance",
     "planck_law",
+    "rayleigh_jeans_law",
+    "sd_rayleigh_jeans",
 ]
 __all__ += [
     "LMS_ConeFundamentals",
