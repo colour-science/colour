e44216252ed82f92fd35b33634c83f7cb47a800b
Thomas Mansencal <thomas.mansencal@gmail.com>
2022-02-26 07:31:04 +1300

    Fix incorrect "Numpy" require in "pyproject.toml" file.

--- tasks.py
+++ tasks.py
@@ -458,7 +458,6 @@ setup({0}
         ),
         source,
     )
-    source = re.sub('{.*extra == "read-the-docs".*', "{", source)
     source = re.sub(
         "setup_kwargs = {(.*)}.*setup\\(\\*\\*setup_kwargs\\)",
         sub_callable,
