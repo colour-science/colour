bb5e265a9264ca013dfff4ff6c69a54e0c003824
Thomas Mansencal <thomas.mansencal@gmail.com>
2022-03-02 13:11:41 +1300

    Ensure that "temperature" parameter can be broadcasted in "colour.planck_law" definition.

--- colour/examples/colorimetry/examples_blackbody.py
+++ colour/examples/colorimetry/examples_blackbody.py
@@ -6,11 +6,11 @@ from colour.utilities import message_box
 message_box("Blackbody / Planckian Radiator Computations")
 
 message_box(
-    "Computing the spectral distribution of a blackbody at temperature 5500K"
+    "Computing the spectral distribution of a blackbody at temperature 5000K"
     'degrees and converting to "CIE XYZ" tristimulus values.'
 )
 cmfs = colour.MSDS_CMFS["CIE 1931 2 Degree Standard Observer"]
-sd_blackbody = colour.sd_blackbody(5500, cmfs.shape)
+sd_blackbody = colour.sd_blackbody(5000, cmfs.shape)
 print(sd_blackbody)
 XYZ = colour.sd_to_XYZ(sd_blackbody, cmfs)
 print(XYZ)
@@ -19,7 +19,7 @@ print("\n")
 
 message_box(
     "Computing the spectral radiance of a blackbody at wavelength 500nm and "
-    "temperature 5500K degrees."
+    "temperature 5000K degrees."
 )
-print(colour.colorimetry.blackbody_spectral_radiance(500 * 1e-9, 5500))
-print(colour.colorimetry.planck_law(500 * 1e-9, 5500))
+print(colour.colorimetry.blackbody_spectral_radiance(500 * 1e-9, 5000))
+print(colour.colorimetry.planck_law(500 * 1e-9, 5000))
