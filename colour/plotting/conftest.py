676c57cb96398a95ab2ef66b19da803a8af559a8
Tucker Downs <tucker@tuckerd.info>
2022-06-01 22:33:47 -0700

    Use conftest to set MPL backend during pytest.
    Prevents window generation when trying to run tests in vscode

---
+++ colour/plotting/conftest.py
@@ -0,0 +1,29 @@
+"""
+Plotting - Tests
+================
+Configures pytest to use the AGG headless backend. This allows the plotting
+unittests to run without creating windows in IDEs such as VSCode.
+"""
+import pytest
+import matplotlib
+
+__author__ = "Colour Developers"
+__copyright__ = "Copyright 2013 Colour Developers"
+__license__ = "New BSD License - https://opensource.org/licenses/BSD-3-Clause"
+__maintainer__ = "Colour Developers"
+__email__ = "colour-developers@colour-science.org"
+__status__ = "Production"
+
+
+@pytest.fixture(autouse=True, scope="session")
+def mplHeadlessBackend() -> None:
+    """
+    Configure matplotlib for headless testing.
+
+    pytest Fixture automatically applied to any tests in this package or any
+    subpackages at the beginning of the pytest session.
+    """
+    curBackend = matplotlib.get_backend()
+    matplotlib.use("AGG")
+    yield
+    matplotlib.use(curBackend)
