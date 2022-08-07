542bf8e24536c39e8630a48067498b4ce7d473ad
François-Xavier Thomas <fxthomas@users.noreply.github.com>
2022-08-07 11:56:56 +0200

    Add inverse-OETFs to video transfer functions from ITU-T H.273

--- colour/models/rgb/transfer_functions/itut_h_273.py
+++ colour/models/rgb/transfer_functions/itut_h_273.py
@@ -14,6 +14,7 @@ References
 
 import numpy as np
 
+from colour.algebra import spow
 from colour.utilities import (
     as_float,
     from_range_1,
@@ -31,6 +32,9 @@ __all__ = [
     "oetf_linear",
     "oetf_log",
     "oetf_log_sqrt",
+    "oetf_inverse_linear",
+    "oetf_inverse_log",
+    "oetf_inverse_log_sqrt",
 ]
 
 
@@ -79,6 +83,52 @@ def oetf_linear(Lc):
     return as_float(from_range_1(V))
 
 
+def oetf_inverse_linear(V):
+    """
+    Linear inverse-opto-electronic transfer function (OETF) defined in ITU-T
+    H.273.
+
+    Parameters
+    ----------
+    V
+        Electrical signal :math:`V`.
+
+    Returns
+    -------
+    :class:`numpy.floating` or :class:`numpy.ndarray`
+        Corresponding scene *Luminance* :math:`Lc`.
+
+    Notes
+    -----
+    +------------+-----------------------+---------------+
+    | **Range**  | **Scale - Reference** | **Scale - 1** |
+    +============+=======================+===============+
+    | ``V``      | [0, 1]                | [0, 1]        |
+    +------------+-----------------------+---------------+
+
+    +------------+-----------------------+---------------+
+    | **Domain** | **Scale - Reference** | **Scale - 1** |
+    +============+=======================+===============+
+    | ``Lc``     | [0, 1]                | [0, 1]        |
+    +------------+-----------------------+---------------+
+
+    References
+    ----------
+    :cite:`ITU2021`
+
+    Examples
+    --------
+    >>> oetf_inverse_linear(0.18)  # doctest: +ELLIPSIS
+    0.17999999...
+    """
+
+    V = to_domain_1(V)
+
+    Lc = V
+
+    return as_float(from_range_1(Lc))
+
+
 def oetf_log(Lc):
     """
     Define the opto-electronic transfer functions (OETF) for Logarithmic
@@ -116,6 +166,12 @@ def oetf_log(Lc):
     --------
     >>> oetf_log(0.18)  # doctest: +ELLIPSIS
     0.6276362525516...
+    >>> oetf_log(0.01)  # doctest: +ELLIPSIS
+    0.0
+    >>> oetf_log(0.001)  # doctest: +ELLIPSIS
+    0.0
+    >>> oetf_log(1.0)  # doctest: +ELLIPSIS
+    1.0
     """
 
     Lc = to_domain_1(Lc)
@@ -129,6 +185,62 @@ def oetf_log(Lc):
     )
 
     return as_float(from_range_1(V))
+
+
+def oetf_inverse_log(V):
+    """
+    Define the inverse-opto-electronic transfer functions (OETF) for
+    Logarithmic encoding (100:1 range) defined in ITU-T H.273.
+
+    Parameters
+    ----------
+    V
+        Electrical signal :math:`V`.
+
+    Returns
+    -------
+    :class:`numpy.floating` or :class:`numpy.ndarray`
+        Corresponding scene *Luminance* :math:`Lc`.
+
+    Notes
+    -----
+    +------------+-----------------------+---------------+
+    | **Range**  | **Scale - Reference** | **Scale - 1** |
+    +============+=======================+===============+
+    | ``V``      | [0, 1]                | [0, 1]        |
+    +------------+-----------------------+---------------+
+
+    +------------+-----------------------+---------------+
+    | **Domain** | **Scale - Reference** | **Scale - 1** |
+    +============+=======================+===============+
+    | ``Lc``     | [0, 1]                | [0, 1]        |
+    +------------+-----------------------+---------------+
+
+    References
+    ----------
+    :cite:`ITU2021`
+
+    Examples
+    --------
+    >>> oetf_inverse_log(0.6276362525516)  # doctest: +ELLIPSIS
+    0.17999999...
+    >>> oetf_inverse_log(0.0)  # doctest: +ELLIPSIS
+    0.01
+    >>> oetf_inverse_log(1.0)  # doctest: +ELLIPSIS
+    1.0
+    """
+
+    V = to_domain_1(V)
+
+    Lc = np.where(
+        V >= 0.0,
+        # Lc in [0.01, 1] range
+        spow(10, (V-1.0)*2.0),
+        # Lc in [0, 0.01] range
+        0.01,
+    )
+
+    return as_float(from_range_1(Lc))
 
 
 def oetf_log_sqrt(Lc):
@@ -168,16 +280,78 @@ def oetf_log_sqrt(Lc):
     --------
     >>> oetf_log_sqrt(0.18)  # doctest: +ELLIPSIS
     0.702109002041...
+    >>> oetf_log_sqrt(0.003162277660168)  # doctest: +ELLIPSIS
+    0.0
+    >>> oetf_log_sqrt(0.0001)  # doctest: +ELLIPSIS
+    0.0
+    >>> oetf_log_sqrt(1.0)  # doctest: +ELLIPSIS
+    1.0
     """
 
     Lc = to_domain_1(Lc)
 
     V = np.where(
         Lc >= np.sqrt(10) / 1000,
-        # Lc in [0.01, 1] range
+        # Lc in [sqrt(10)/1000, 1] range
         1.0 + np.log10(Lc) / 2.5,
-        # Lc in [0, 0.01] range
+        # Lc in [0, sqrt(10)/1000] range
         0.0,
     )
 
     return as_float(from_range_1(V))
+
+
+def oetf_inverse_log_sqrt(V):
+    """
+    Define the inverse-opto-electronic transfer functions (OETF) for
+    Logarithmic encoding (100*Sqrt(10):1 range) defined in ITU-T H.273.
+
+    Parameters
+    ----------
+    V
+        Electrical signal :math:`V`.
+
+    Returns
+    -------
+    :class:`numpy.floating` or :class:`numpy.ndarray`
+        Corresponding scene *Luminance* :math:`Lc`.
+
+    Notes
+    -----
+    +------------+-----------------------+---------------+
+    | **Range**  | **Scale - Reference** | **Scale - 1** |
+    +============+=======================+===============+
+    | ``V``      | [0, 1]                | [0, 1]        |
+    +------------+-----------------------+---------------+
+
+    +------------+-----------------------+---------------+
+    | **Domain** | **Scale - Reference** | **Scale - 1** |
+    +============+=======================+===============+
+    | ``Lc``     | [0, 1]                | [0, 1]        |
+    +------------+-----------------------+---------------+
+
+    References
+    ----------
+    :cite:`ITU2021`
+
+    Examples
+    --------
+    >>> oetf_inverse_log_sqrt(0.702109002041)  # doctest: +ELLIPSIS
+    0.1799999999...
+    >>> oetf_inverse_log_sqrt(0.0)  # doctest: +ELLIPSIS
+    0.00316227766...
+    >>> oetf_inverse_log_sqrt(1.0)  # doctest: +ELLIPSIS
+    1.0
+    """
+
+    V = to_domain_1(V)
+
+    Lc = np.where(
+        V >= 0.0,
+        # Lc in [sqrt(10)/1000, 1] range
+        spow(10, (V-1.0)*2.5),
+        # Lc in [0, sqrt(10)/1000] range
+        np.sqrt(10) / 1000,
+    )
+
+    return as_float(from_range_1(Lc))
