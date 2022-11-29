1cf0025815948cdec4a1cc94dbf215379c3c9d19
Thomas Mansencal <thomas.mansencal@gmail.com>
2022-11-29 21:17:52 +1300

    Update "conf.py" file.

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ extensions = [
 ]
 
 intersphinx_mapping = {
-    "python": ("https://docs.python.org/3.8", None),
+    "python": ("https://docs.python.org/3.11", None),
     "matplotlib": ("https://matplotlib.org/stable", None),
     "numpy": ("https://numpy.org/doc/stable", None),
     "pandas": ("https://pandas.pydata.org/pandas-docs/dev", None),
