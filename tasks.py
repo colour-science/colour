8933cc332592e815e5afba1581daa9650e40a548
Thomas Mansencal <thomas.mansencal@gmail.com>
2023-03-09 22:20:32 +1300

    Ensure that coverage data is properly captured with distributed unit tests.

--- tasks.py
+++ tasks.py
@@ -212,6 +212,7 @@ def tests(ctx: Context):
         "pytest "
         "--doctest-modules "
         f"--ignore={PYTHON_PACKAGE_NAME}/examples "
+        f"--cov={PYTHON_PACKAGE_NAME} "
         f"{PYTHON_PACKAGE_NAME}"
     )
 
