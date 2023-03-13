b44ea1182ac05b80f30568b57f99b0dd758342b2
Tucker Downs <tucker@roevisual.us>
2023-03-13 19:45:59 -0400

    cache the interval results... occasional help.

--- colour/utilities/array.py
+++ colour/utilities/array.py
@@ -53,6 +53,7 @@ from colour.utilities import (
     suppress_warnings,
     validate_method,
 )
+from colour.utilities.common import CACHE_REGISTRY
 
 __author__ = "Colour Developers"
 __copyright__ = "Copyright 2013 Colour Developers"
@@ -1863,6 +1864,11 @@ def closest(a: ArrayLike, b: ArrayLike) -> NDArray:
     return a[closest_indexes(a, b)]
 
 
+_CACHE_DISTRIBUTION_INTERVAL: dict = CACHE_REGISTRY.register_cache(
+    f"{__name__}._CACHE_DISTRIBUTION_INTERVAL"
+)
+
+
 def interval(distribution: ArrayLike, unique: bool = True) -> NDArray:
     """
     Return the interval size of given distribution.
@@ -1900,13 +1906,23 @@ def interval(distribution: ArrayLike, unique: bool = True) -> NDArray:
     """
 
     distribution = as_float_array(distribution)
+    hash_key = hash(
+        (distribution.tobytes(), np.array(distribution.shape).tobytes())
+    )
+    if hash_key in _CACHE_DISTRIBUTION_INTERVAL:
+        return np.copy(_CACHE_DISTRIBUTION_INTERVAL[hash_key])
 
     differences = np.abs(distribution[1:] - distribution[:-1])
 
-    if unique:
-        return np.unique(differences)
+    if unique and (differences == differences[0]).all():
+        out = np.array([differences[0]])
+    elif unique:
+        out = np.unique(differences)
     else:
-        return differences
+        out = differences
+
+    _CACHE_DISTRIBUTION_INTERVAL[hash_key] = np.copy(out)
+    return out
 
 
 def is_uniform(distribution: ArrayLike) -> bool:
