699dfb31dca8b9d3149f865b4b34659386b9f767
Thomas Mansencal <thomas.mansencal@gmail.com>
2023-11-22 21:42:51 +1300

    Fix `Fröhlich` non-word letter.
    References https://github.com/codespell-project/codespell/issues/3227.

--- colour/models/icacb.py
+++ colour/models/icacb.py
@@ -9,7 +9,7 @@ Defines the :math:`IC_AC_B` colourspace transformations:
 
 References
 ----------
--   :cite:`Frohlich2017` : Fröhlich, J. (2017). Encoding high dynamic range
+-   :cite:`Frohlich2017` : Fröhlich, J. (2017). Encoding high dynamic range
     and wide color gamut imagery. doi:10.18419/OPUS-9664
 """
 
