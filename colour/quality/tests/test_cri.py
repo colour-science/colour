901a609bde89624ea9f9c6c117bbfe966d7475bf
Christoph Müllner <christophm30@gmail.com>
2025-03-06 00:48:08 +0100

    datasets: TCS: Adding TCS15
    TCS15 was added to the CIE 1995 test color samples to get a better
    representation of the Japanese skin complexion.
    This commit adds this color sample and adjust the CRI test file
    to pass.
    
    Note, that the TCS15 dataset is published from 380-780 nm in 5 nm increments.
    This differs from all other TCS data, which is published from 360-830 nm
    (also in 5 nm increments).
    
    The integration of TCS15 in this commit is breaking existing code,
    which can be observed in the following regressions:
    * FAILED colour/colorimetry/tests/test_uniformity.py::TestSpectralUniformity::test_spectral_uniformity
    * FAILED colour/colorimetry/uniformity.py::colour.colorimetry.uniformity.spectral_uniformity
    
    The integration is addressed in a following commit.
    
    Source of data is the CIE's open access dataset page:
    https://www.cie.co.at/datatable/spectral-radiance-factors-test-colour-sample-15-japanese-skin-complexion-5nm-wavelength
    
    Data source reference:
    CIE 2024, Spectral radiance factors of test-colour sample #15 of the Japanese
    skin complexion, 5nm wavelength steps, International Commission on Illumination
    (CIE), Vienna, AT, DOI: 10.25039/CIE.DS.7chm7z5h
    
    Signed-off-by: Christoph Müllner <christophm30@gmail.com>

--- colour/quality/tests/test_cri.py
+++ colour/quality/tests/test_cri.py
@@ -158,6 +158,7 @@ class TestColourRenderingIndex:
                 12: DataColourQualityScale_TCS(name="TCS12", Q_a=74.890093866757994),
                 13: DataColourQualityScale_TCS(name="TCS13", Q_a=72.771930354944615),
                 14: DataColourQualityScale_TCS(name="TCS14", Q_a=94.884867470552663),
+                15: DataColourQualityScale_TCS(name="TCS15", Q_a=59.613744524909143),
             },
             colorimetry_data=(
                 (
@@ -245,6 +246,12 @@ class TestColourRenderingIndex:
                         uv=np.array([0.18328188, 0.35214117]),
                         UVW=np.array([-6.11563143, 19.91896684, 40.34566797]),
                     ),
+                    DataColorimetry_TCS(
+                        name="TCS15",
+                        XYZ=np.array([31.87001132, 31.71897232, 22.84936407]),
+                        uv=np.array([0.22124167, 0.33028974]),
+                        UVW=np.array([20.88976213, 12.62627126, 62.13702346]),
+                    ),
                 ),
                 (
                     DataColorimetry_TCS(
@@ -330,6 +337,12 @@ class TestColourRenderingIndex:
                         XYZ=np.array([9.34469915, 11.70922060, 5.33442353]),
                         uv=np.array([0.18597686, 0.34955284]),
                         UVW=np.array([-6.34991066, 18.99712303, 39.76962229]),
+                    ),
+                    DataColorimetry_TCS(
+                        name="TCS15",
+                        XYZ=np.array([35.0651964, 32.69237678, 24.25024725]),
+                        uv=np.array([0.23447077, 0.32790662]),
+                        UVW=np.array([29.62847425, 12.35345214, 62.93841026]),
                     ),
                 ),
             ),
